c666daf0949aaa19502e91b2d7ac922ba8a16e45
Sena <love-diving@hotmail.com>
2016-09-23 09:55:56 +1200

    share done

--- js/main.js
+++ js/main.js
@@ -72,3 +72,11 @@
               map.fitBounds(bounds);
           });
       }
+
+     (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7&appId=678921105608179";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
