8d1cb082b876c05eb9e11b449b8c1d99b13233cc
Sena <love-diving@hotmail.com>
2016-09-22 23:58:32 +1200

    search

--- js/main.js
+++ js/main.js
@@ -1,28 +1,66 @@
-// Get jquery objects from DOM
-var imgSelector = $("#my-file-selector");
-var refreshbtn = $("#refreshbtn");
-var pageheader = $("#page-header");
-var pagecontainer = $("#page-container");
+var app = angular.module('myApp', []);
 
-// Register event listeners
-imgSelector.on("change", function () {
-    pageheader.html("Just a sec while we analyse your mood...");
-    setTimeout(changeUI, 2000); //some external call would happen here, add 2 sec delay to simulate
-                                //can implement jquery loading plugin
+app.service('Map', function($q) {
+    
+    this.init = function() {
+        var options = {
+            center: new google.maps.LatLng(40.7127837, -74.00594130000002),
+            zoom: 13,
+            disableDefaultUI: true    
+        }
+        this.map = new google.maps.Map(
+            document.getElementById("map"), options
+        );
+        this.places = new google.maps.places.PlacesService(this.map);
+    }
+    
+    this.search = function(str) {
+        var d = $q.defer();
+        this.places.textSearch({query: str}, function(results, status) {
+            if (status == 'OK') {
+                d.resolve(results[0]);
+            }
+            else d.reject(status);
+        });
+        return d.promise;
+    }
+    
+    this.addMarker = function(res) {
+        if(this.marker) this.marker.setMap(null);
+        this.marker = new google.maps.Marker({
+            map: this.map,
+            position: res.geometry.location,
+            animation: google.maps.Animation.DROP
+        });
+        this.map.setCenter(res.geometry.location);
+    }
+    
 });
-refreshbtn.on("click", function () {
-    // Load random song based on mood
-    alert("You clicked the button"); //can demo with sweetAlert plugin
+
+app.controller('newPlaceCtrl', function($scope, Map) {
+    
+    $scope.place = {};
+    
+    $scope.search = function() {
+        $scope.apiError = false;
+        Map.search($scope.searchPlace)
+        .then(
+            function(res) { // success
+                Map.addMarker(res);
+                $scope.place.name = res.name;
+                $scope.place.lat = res.geometry.location.lat();
+                $scope.place.lng = res.geometry.location.lng();
+            },
+            function(status) { // error
+                $scope.apiError = true;
+                $scope.apiStatus = status;
+            }
+        );
+    }
+    
+    $scope.send = function() {
+        alert($scope.place.name + ' : ' + $scope.place.lat + ', ' + $scope.place.lng);    
+    }
+    
+    Map.init();
 });
-
-// Manipulate the DOM
-function changeUI() {
-    //Show detected mood
-    pageheader.html("Your mood is: ...");
-
-    //Display song refresh button
-    refreshbtn.css("display", "inline");
-
-    //Remove offset at the top
-    pagecontainer.css("marginTop", "20px");
-};
