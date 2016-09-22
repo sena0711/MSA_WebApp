917fd72fd4a72a669b3b93c64400f2dc2acd9cc2
Sena <love-diving@hotmail.com>
2016-09-23 04:30:24 +1200

    Init from Auckland

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
 
       function initAutocomplete() {
           var map = new google.maps.Map(document.getElementById('map'), {
-              center: { lat: -33.8688, lng: 151.2195 },
+              center: { lat: -36.8484597, lng: 174.76333150000005 },
               zoom: 13,
               mapTypeId: 'roadmap'
           });
