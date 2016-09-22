5cb30a5e9d9d61d2385a64127e8743ca59e534fa
Sena <love-diving@hotmail.com>
2016-09-23 01:15:30 +1200

    testing to fix map

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 var app = angular.module('myApp', []);
 
-function initMap() {
-        var mapDiv = document.getElementById('map');
-        var map = new google.maps.Map(mapDiv, {
-            center: {lat: -36.8484597, lng: -74.00594130000002},
-            zoom: 13
-        });
-      }
-
-
 
 
 
